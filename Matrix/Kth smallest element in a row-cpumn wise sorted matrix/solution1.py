4dfb0fbdc7dad7f0329ff4dc44587d1ca7c08cee
Tejas07PSK <palashsarkar0007@gmail.com>
2021-12-02 08:35:40 +0530

    more optimized manual heap top down !!

--- Matrix/Kth smallest element in a row-cpumn wise sorted matrix/solution1.py
+++ Matrix/Kth smallest element in a row-cpumn wise sorted matrix/solution1.py
@@ -13,20 +13,21 @@ class MinHeap:
             i += 1
         self.__last_idx = N - 1
 
-    def __swap(self, i, j):
+    def __swap (self, i, j):
         temp = self.__heap_arr[i]
         self.__heap_arr[i] = self.__heap_arr[j]
         self.__heap_arr[j] = temp
 
-    def delete (self):
-        res = self.__heap_arr[0]
-        self.__heap_arr[0] = self.__heap_arr[self.__last_idx]
+    def pop (self):
+        return self.__heap_arr[0]
+
+    def heapify (self):
         i = 0
-        while (i < self.__last_idx):
+        while (i <= self.__last_idx):
             left, right = ((2 * i) + 1), ((2 * i) + 2)
-            left_ele = self.__heap_arr[left]["element"] if (left < self.__last_idx) else math.inf
-            right_ele = self.__heap_arr[right]["element"] if (right < self.__last_idx) else math.inf
-            if ((left >= self.__last_idx) and (right >= self.__last_idx)) or ((self.__heap_arr[i]["element"] <= left_ele) and (self.__heap_arr[i]["element"] <= right_ele)):
+            left_ele = self.__heap_arr[left]["element"] if (left <= self.__last_idx) else math.inf
+            right_ele = self.__heap_arr[right]["element"] if (right <= self.__last_idx) else math.inf
+            if ((left > self.__last_idx) and (right > self.__last_idx)) or ((self.__heap_arr[i]["element"] <= left_ele) and (self.__heap_arr[i]["element"] <= right_ele)):
                 break
             if (left_ele <= right_ele):
                 self.__swap(i, left)
@@ -34,21 +35,17 @@ class MinHeap:
             else:
                 self.__swap(i, right)
                 i = right
-        return res
 
     def insert (self, obj):
-        self.__heap_arr[self.__last_idx] = obj
-        i = self.__last_idx
-        while ((i > 0) and (self.__heap_arr[i]["element"] < self.__heap_arr[(i - 1) // 2]["element"])):
-            self.__swap(i, ((i - 1) // 2))
-            i = (i - 1) // 2
+        self.__heap_arr[0] = obj
+        self.heapify()
 
 def kthSmallest (mat, N, K):
     mh = MinHeap(0, mat[0], N)
     op = {}
     i = 0
     while (i < K):
-        op = mh.delete()
+        op = mh.pop()
         mh.insert({
             "element": mat[op["row"] + 1][op["column"]] if ((op["row"] + 1) < N) else math.inf,
             "row": op["row"] + 1,
