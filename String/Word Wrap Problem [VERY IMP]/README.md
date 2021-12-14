6495cb0b7365fa098a004c963e42b8ba59bcab18
Tejas07PSK <palashsarkar0007@gmail.com>
2021-12-15 02:17:55 +0530

    commit !!

--- String/Word Wrap Problem [VERY IMP]/README.md
+++ String/Word Wrap Problem [VERY IMP]/README.md
@@ -6,5 +6,5 @@
 - **__GeeksForGeeks__** :point_right: https://practice.geeksforgeeks.org/problems/word-wrap1646/1
 
 ### Solution
-- **_solution1.py_** :point_right: use top down dp, with memoization **time-complexity O(N^2)**, **space-complexity O(N)**.
-- **_solution2.py_** :point_right: use bottom up dp, with tabulation **time-complexity O(N^2)**, **space-complexity O(N^2)**.
+- **_solution1.py_** :point_right: use top down dp, with memoization/recursion **time-complexity O(N^2)**, **space-complexity O(N)**.
+- **_solution2.py_** :point_right: use bottom up dp, with tabulation/iteration **time-complexity O(N^2)**, **space-complexity O(N)**.
--- String/Word Wrap Problem [VERY IMP]/solution2.py
+++ String/Word Wrap Problem [VERY IMP]/solution2.py
@@ -1,40 +1,23 @@
 import math
 class Solution:
-    def preparePossibleCostsMatrix (self, nums, n, k):
-        i, mat = 0, [[math.inf for j in range(n)] for i in range(n)]
-        while (i < n):
-            j, sum_so_far = i, 0
-            while (j < n):
-                sum_so_far += nums[j]
-                if ((sum_so_far + (j - i)) <= k):
-                    mat[i][j] = ((k - (sum_so_far + (j - i))) ** 2) if (j < (n - 1)) else 0
-                else:
-                    break
-                j += 1
-            i += 1
-        return mat
-
     def solveWordWrap (self, nums, k):
         n = len(nums)
-        possible_costs_mat = self.preparePossibleCostsMatrix(nums, n, k)
-        #print(possible_costs_mat)
-        dp = [inf for i in range(n)]
-        #arrangement = [inf for i in range(n)]
+        dp = [math.inf for i in range(n)]
         dp[n - 1] = 0
         i = n - 2
         while (i >= 0):
-            j = n - 1
-            while ((possible_costs_mat[i][j] == math.inf) and (possible_costs_mat[i][j - 1] == math.inf)):
-                j -= 1
-            dp[i] = possible_costs_mat[i][j]
-            #arrangement[i] = j
-            while (j > i):
-                temp = possible_costs_mat[i][j - 1] + dp[j]
-                if (temp < dp[i]):
-                    dp[i] = temp
-                    #arrangement[i] = j
-                j -= 1
+            j = i
+            sum_of_words, min_so_far = 0, math.inf
+            while (j < n):
+                sum_of_words += nums[j]
+                if ((sum_of_words + (j - i)) <= k):
+                    temp = 0
+                    if (j < (n - 1)):
+                        temp = ((k - (sum_of_words + (j - i))) ** 2) + dp[j + 1]
+                    min_so_far = min(min_so_far, temp)
+                else:
+                    break
+                j += 1
+            dp[i] = min_so_far
             i -= 1
-        #print(dp)
-        #print(arrangement)
         return (dp[0])
