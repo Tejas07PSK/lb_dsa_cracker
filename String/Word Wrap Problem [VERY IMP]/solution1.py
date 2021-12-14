7c8ee3754d66156275df18f25edd81dafed23318
Tejas07PSK <palashsarkar0007@gmail.com>
2021-12-15 01:43:29 +0530

    commit !!

--- String/Word Wrap Problem [VERY IMP]/solution1.py
+++ String/Word Wrap Problem [VERY IMP]/solution1.py
@@ -2,27 +2,26 @@ import math
 class Solution:
     def dp (self, nums, i, n, k, memo):
         if (i == n - 1):
-            if (memo[i][i] == math.inf):
-                memo[i][i] = 0
-            return memo[i][i]
-        if (memo[i][i] != math.inf):
-            return memo[i][i]
-        min_so_far = (((k - nums[i]) ** 2) + self.dp(nums, (i + 1), n, k, memo))
-        sum_of_words = nums[i]
-        for j in range((i + 1), n):
+            if (memo[i] == math.inf):
+                memo[i] = 0
+            return memo[i]
+        if (memo[i] != math.inf):
+            return memo[i]
+        min_so_far = math.inf
+        sum_of_words = 0
+        for j in range(i, n):
             sum_of_words += nums[j]
             if ((sum_of_words + (j - i)) <= k):
+                temp = 0
                 if (j < (n - 1)):
-                    memo[i][j] = (((k - (sum_of_words + (j - i))) ** 2) + (self.dp(nums, (j + 1), n, k, memo)))
-                else:
-                    memo[i][j] = 0
-                min_so_far = min(min_so_far, memo[i][j])
+                    temp = (((k - (sum_of_words + (j - i))) ** 2) + (self.dp(nums, (j + 1), n, k, memo)))
+                min_so_far = min(min_so_far, temp)
             else:
                 break
-        memo[i][i] = min_so_far
-        return memo[i][i]
+        memo[i] = min_so_far
+        return memo[i]
 
     def solveWordWrap (self, nums, k):
         n = len(nums)
-        memo = [[math.inf for j in range(n)] for i in range(n)]
+        memo = [math.inf for j in range(n)]
         return self.dp(nums, 0, n, k, memo)
