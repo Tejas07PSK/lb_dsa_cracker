645cfc7bdfb22cdfa611fa96bce2c4b9f9db33f0
Palash Sarkar <palashsarkar0007@gmail.com>
2022-02-19 04:21:17 +0530

    commit !!
    commit !!

--- Searching & Sorting/Job Scheduling Algo/solution1.py
+++ Searching & Sorting/Job Scheduling Algo/solution1.py
@@ -1,8 +1,11 @@
 class Solution:
-    def JobScheduling (self, N, Jobs):
-        Jobs.sort(key=lambda x: x[1]) ; max_profit = Jobs[0][2] ; profit_calc_arr = [Jobs[i][2] for i in range(N)]
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        jobs = list(zip(startTime, endTime, profit)) ; jobs.sort(key=lambda x : x[1]) ; N = len(jobs)
+        profit_calc_arr = [0 for i in range(N)] ;  profit_calc_arr[0] = jobs[0][2]
         for i in range(1, N):
-            for j in range(0, i):
-                if (Jobs[j][1] < Jobs[i][0]): profit_calc_arr[i] = max(profit_calc_arr[i], (profit_calc_arr[j] + Jobs[i][2]))
-            max_profit = max(max_profit, profit_calc_arr[i])
-        return max_profit
+            profit_calc_arr[i] = jobs[i][2]
+            for j in range(i - 1, -1, -1):
+                if (jobs[j][1] <= jobs[i][0]):
+                    profit_calc_arr[i] += profit_calc_arr[j] ; break
+            profit_calc_arr[i] = max(profit_calc_arr[i], profit_calc_arr[i - 1])
+        return profit_calc_arr[-1]
