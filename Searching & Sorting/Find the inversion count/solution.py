5efdee74c016a69fac9d0bba23997cc03c01575e
Palash Sarkar <palashsarkar0007@gmail.com>
2022-02-24 01:24:01 +0530

    commit !!
    commit !!

--- Searching & Sorting/Find the inversion count/solution.py
+++ Searching & Sorting/Find the inversion count/solution.py
@@ -7,7 +7,7 @@ class Solution:
         return self.__inv_cnt
 
     def __enhcMergeSort(self, left, right, arr):
-        size = right - left + 1
+        size = (right - left + 1)
         if (size < 2): return
         if (size == 2):
             if (arr[left] > arr[right]):
@@ -22,15 +22,12 @@ class Solution:
         tmp_arr = [0 for i in range(left, (right + 1))] ; i, j, cntr = left, (mid + 1), 0
         while ((i <= mid) and (j <= right)):
             if (arr[i] > arr[j]):
-                tmp_arr[cntr] = arr[j] ; j += 1 ; self.__inv_cnt += 1
-            elif (arr[i] < arr[j]):
-                tmp_arr[cntr] = arr[i] ; i += 1
-            else:
-                tmp_arr[cntr] = arr[i] ; i += 1 ; cntr += 1
                 tmp_arr[cntr] = arr[j] ; j += 1
+            else:
+                self.__inv_cnt += (j - mid - 1) ; tmp_arr[cntr] = arr[i] ; i += 1
             cntr += 1
         while (i <= mid):
-            tmp_arr[cntr] = arr[i] ; i += 1 ; cntr += 1 ; self.__inv_cnt += 1
+            tmp_arr[cntr] = arr[i] ; i += 1 ; cntr += 1 ; self.__inv_cnt += (j - mid - 1)
         while (j <= right):
             tmp_arr[cntr] = arr[j] ; j += 1 ; cntr += 1
         i, cntr = left, 0
