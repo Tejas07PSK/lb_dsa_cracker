774f355496da19b005d5fefc5cc686de101ddf56
Palash Sarkar <palashsarkar0007@gmail.com>
2022-02-26 09:19:42 +0530

    commit !!
    commit !!

--- LinkedList/Reverse a Linked List in group of Given Size/solution.py
+++ LinkedList/Reverse a Linked List in group of Given Size/solution.py
@@ -1,24 +1,30 @@
+"""Return reference of new head of the reverse linked list
+  The input list will have at least one element
+  Node is defined as
+
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+  This is method only submission.
+  You only need to complete the method.
+"""
 class Solution:
     def reverse (self, ll_head, k):
         if ((ll_head == None) or (k == 1)): return ll_head
-        prev_start, start = None, None
-        prev, curr, after, start = None, ll_head, None, None
-        i, cnt = 0, 0
+        prev, curr, after, i = None, ll_head, None, 1
+        while ((curr != None) and (i <= k)):
+            after = curr.next ; curr.next = prev
+            prev = curr ; curr = after ; i += 1
+        prev_start, start = ll_head, curr
+        ll_head = prev ; prev = None ; i = 1
         while (curr != None):
-            i += 1
-            if (i == 1):
+            if ((i == k) or (curr.next == None)):
+                prev_start.next = curr ; after = curr.next
+                curr.next = prev ; prev = None
+                curr = after ; i = 1
                 prev_start = start ; start = curr
-                prev = curr ; curr = curr.next
-            else: 
-                if (i == k):
-                    if (cnt == 0):
-                        ll_head = curr ; cnt += 1
-                    if (prev_start != None): prev_start.next = curr
-                    i = 0
-                after = curr.next ; curr.next = prev ; prev = curr; curr = after
-        start.next = None
-        if (i < k):
-            if (cnt == 0):
-                ll_head = prev ; cnt += 1
-            if (prev_start != None): prev_start.next = prev
+            else:
+                after = curr.next ; curr.next = prev
+                prev = curr ; curr = after ; i += 1
         return ll_head
