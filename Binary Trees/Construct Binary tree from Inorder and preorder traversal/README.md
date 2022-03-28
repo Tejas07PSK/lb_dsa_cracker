61e3772aa1ace9e11af5c8269929933cf6b295e2
Palash Sarkar <palashsarkar0007@gmail.com>
2022-03-28 06:43:01 +0530

    commit !!
    commit !!

---
+++ Binary Trees/Construct Binary tree from Inorder and preorder traversal/README.md
@@ -0,0 +1,10 @@
+# Construct binary tree from Inorder and Preorder traversal
+
+---
+
+### Problem Links
+- **__GeeksForGeeks__** :point_right: https://practice.geeksforgeeks.org/problems/construct-tree-1/1
+- **__LeetCode__** :point_right: https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+
+### Solution
+- **_solution.py_** :point_right: very complicated, look at code, revise a lot, use stack with some ad-hoc logic **time-complexity O(N)**, **space-complexity O(N)**.
--- Binary Trees/Construct Binary tree from Inorder and preorder traversal/solution.py
+++ Binary Trees/Construct Binary tree from Inorder and preorder traversal/solution.py
@@ -1,34 +1,27 @@
-class Solution
-{
-    static  HashMap<Integer,Integer>g=new HashMap<>();
-    static int index;
-    static Node sub(int in[],int pre[],int ins,int ind)
-   {
-       if(index>=in.length||ins>ind)
-         return null;
-        int element=pre[index];
-        index++;
-        int posi=g.get(element);
-        Node root=new Node(element);
-       root.left=sub(in,pre,ins,posi-1);
-       root.right=sub(in,pre,posi+1,ind);
-       return root;
-       
-         
-   }
-   public static Node buildTree(int in[], int pre[], int n)
-   {
-       
-       g.clear();
-       index=0;
-       for(int i=0;i<in.length;i++)
-       {
-           g.put(in[i],i);
-          
-       }
-           
-       Node root=sub(in,pre,0,n-1);
-       return root;
-      
-   }
-}
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+class Solution:
+    def buildTree (self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        pre_order_len, in_order_len = len(preorder), len(inorder)
+        if ((not pre_order_len) or (not in_order_len)): return None
+        p, i, stk, to_right = 0, 0, deque(), False
+        ptr = root = TreeNode(preorder[p]); stk.append(root) ; p += 1
+        while (p < pre_order_len):
+            if ((i < in_order_len) and (stk) and (stk[-1].val == inorder[i])):
+                while ((i < in_order_len) and (stk) and (stk[-1].val == inorder[i])):
+                    ptr = stk.pop() ; i += 1
+                to_right = True
+            if (to_right):
+                ptr.right = TreeNode(preorder[p]) ; ptr = ptr.right
+                stk.append(ptr) ; to_right = False
+            else:
+                ptr.left = TreeNode(preorder[p]) ; ptr = ptr.left
+                stk.append(ptr)
+            p += 1
+        return root
