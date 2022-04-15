ff73f4fc7979ed556e6ca1b2156ab5f45d4d2b32
Palash Sarkar <palashsarkar0007@gmail.com>
2022-04-16 01:05:42 +0530

    commit !!
    commit !!

---
+++ Binary Search Trees/Convert a normal BST into a Balanced BST/README.md
@@ -0,0 +1,9 @@
+# Convert a normal BST into a Balanced BST
+
+---
+
+### Problem Links
+- **__GeeksForGeeks__** :point_right: https://practice.geeksforgeeks.org/problems/normal-bst-to-balanced-bst/1
+
+### Solution
+- **_solution.py_** :point_right: use in-order traversal over given bst and store the result in an array, then use recursive divide and conquer technique over the array to build the min height balanced bst **time-complexity O(N)**, **space-complexity O(N)**.
--- Binary Search Trees/Convert a normal BST into a Balanced BST/solution.py
+++ Binary Search Trees/Convert a normal BST into a Balanced BST/solution.py
@@ -1,7 +1,14 @@
+# Tree Node
+# class Node:
+#     def __init__(self, val):
+#         self.right = None
+#         self.data = val
+#         self.left = None
+
 class Solution:
     def __constructMinHeightBST (self, l, r):
         if (l == r): return Node(self.in_ord_arr[l])
-        mid = l + ((r - l) // 2) ; root = Node(mid)
+        mid = l + ((r - l) // 2) ; root = Node(self.in_ord_arr[mid])
         if ((mid - 1) >= l): root.left = self.__constructMinHeightBST(l, (mid - 1))
         if ((mid + 1) <= r): root.right = self.__constructMinHeightBST((mid + 1), r)
         return root
@@ -11,7 +18,7 @@ class Solution:
         self.in_ord_arr.append(root.data)
         if (root.right != None): self.__inOrderHelper(root.right)
 
-    def buildBalancedBST (self, root):
+    def buildBalancedTree (self, root):
         if (root == None): return root
         self.in_ord_arr = [] ; self.__inOrderHelper(root)
-        return self.__constructMinHeightBST(0, len(self.in_ord_arr))
+        return self.__constructMinHeightBST(0, len(self.in_ord_arr) - 1)
