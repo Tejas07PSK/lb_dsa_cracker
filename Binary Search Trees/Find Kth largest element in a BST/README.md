5029f34f914abcb058103382c05735fec3c65996
Palash Sarkar <palashsarkar0007@gmail.com>
2022-04-16 13:29:45 +0530

    commit !!
    commit !!

---
+++ Binary Search Trees/Find Kth largest element in a BST/README.md
@@ -0,0 +1,9 @@
+# Find Kth largest element in a BST
+
+---
+
+### Problem Links
+- **__GeeksForGeeks__** :point_right: https://practice.geeksforgeeks.org/problems/kth-largest-element-in-bst/1
+
+### Solution
+- **_solution.py_** :point_right: use standard bst reverse in-order traversal, with some ad-hoc logic **time-complexity O(height + k)**, **space-complexity O(height)**.
---
+++ Binary Search Trees/Find Kth largest element in a BST/solution.py
@@ -0,0 +1,20 @@
+# class Node:
+#     def __init__(self, val):
+#         self.data = val
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def __inOrderHelper (self, root, k):
+        if (root == None): return
+        if (root.right != None): self.__inOrderHelper(root.right, k)
+        if (self.res != None): return
+        self.cntr += 1
+        if (self.cntr == k):
+            self.res = root.data ; return
+        if (root.left != None): self.__inOrderHelper(root.left, k)
+
+    def kthLargest (self, root, k):
+        self.cntr, self.res = 0, None
+        self.__inOrderHelper(root, k)
+        return self.res
