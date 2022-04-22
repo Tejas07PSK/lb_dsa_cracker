76d806b9acf480c26ba09ff3832cde97b5bd8c69
Palash Sarkar <palashsarkar0007@gmail.com>
2022-04-22 08:00:40 +0530

    commit !!
    commit !!

--- Binary Search Trees/Check preorder is valid or not/solution.py
+++ Binary Search Trees/Check preorder is valid or not/solution.py
@@ -1,14 +1,21 @@
 from collections import deque
-def isPreorderValid (arr):
-    stk, res = deque(), 1 ; stk.append((arr[0], None))
-    for i in range(1, len(arr)):
-        if (arr[i] <= stk[-1][0]):
-            if ((stk[-1][1] != None) and (stk[-1][0] > stk[-1][1]) and (arr[i] < stk[-1][1])):
-                res = 0 ; break
-            stk.append((arr[i], stk[-1][0]))
-        else:
-            prev = stk.pop()
-            while ((stk) and (arr[i] > stk[-1][0])): prev = stk.pop()
-            stk.append((arr[i], prev[0]))
-    stk.clear()
-    return res
+class Solution:
+    def canRepresentBST (self, arr, N):
+        stk = deque() ; stk.append([arr[0], arr[0], arr[0], None])
+        for i in range(1, len(arr)):
+            if (arr[i] <= stk[-1][0]): stk.append([arr[i], arr[i], arr[i], stk[-1][0]])
+            else:
+                prev_ele, prev_min, prev_max, max_predecessor = stk.pop()
+                while ((stk) and (arr[i] > stk[-1][0]) and (max_predecessor != None) and (arr[i] > max_predecessor)):
+                    if ((prev_ele <= stk[-1][0]) and (stk[-1][0] < prev_max)): return 0
+                    if ((prev_ele > stk[-1][0]) and (stk[-1][0] > prev_min)): return 0
+                    stk[-1][1], stk[-1][2] = min(stk[-1][1], prev_min), max(stk[-1][2], prev_max)
+                    prev_ele, prev_min, prev_max, max_predecessor = stk.pop()
+                stk.append([prev_ele, prev_min, prev_max, max_predecessor]) ; stk.append([arr[i], arr[i], arr[i], max_predecessor])
+        prev_ele, prev_min, prev_max, max_predecessor = stk.pop()
+        while (stk):
+            if ((prev_ele <= stk[-1][0]) and (stk[-1][0] < prev_max)): return 0
+            if ((prev_ele > stk[-1][0]) and (stk[-1][0] > prev_min)): return 0
+            stk[-1][1], stk[-1][2] = min(stk[-1][1], prev_min), max(stk[-1][2], prev_max)
+            prev_ele, prev_min, prev_max, max_predecessor = stk.pop()
+        return 1
