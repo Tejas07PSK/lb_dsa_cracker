e83e70974d04dd8de532f8f62d71eae8cc28a9c7
Palash Sarkar <palashsarkar0007@gmail.com>
2022-05-07 15:52:42 +0530

    commit !!
    commit !!

---
+++ Greedy/Minimize Cash Flow among a given set of friends who have borrowed money from each other/README.md
@@ -0,0 +1,9 @@
+# Minimize Cash Flow among a given set of friends who have borrowed money from each other
+
+---
+
+### Problem Links
+- **__CodeStudio__** :point_right: https://www.codingninjas.com/codestudio/problems/settle-debt_1232658?leftPanelTab=0
+
+### Solution
+- **_solution.py_** :point_right: based on the question requirement you can either use simple greedy with memoization and backtracking, or only backtracking, look at code for more details **time-complexity - greedy O(N^2) - backtracking only O(N!)**, **space-complexity O(N)**.
--- Greedy/Minimize Cash Flow among a given set of friends who have borrowed money from each other/solution.py
+++ Greedy/Minimize Cash Flow among a given set of friends who have borrowed money from each other/solution.py
@@ -1,19 +1,24 @@
 from collections import defaultdict
 from math import inf
-def dfs_backtrack (k, balance_list):
+def dfs_backtrack (k, balance_list, dp_hm):
     while ((k < len(balance_list)) and (balance_list[k] == 0)): k += 1
     if (k >= len(balance_list)): return 0
+    #if (k in dp_hm): return dp_hm[k]   ----> use this only if question asks for greedy solution
     min_trans = inf
     for i in range(k + 1, len(balance_list)):
         if ((balance_list[k] * balance_list[i]) < 0):
             balance_list[i] += balance_list[k]
-            min_trans = min(min_trans, 1 + dfs_backtrack(k + 1, balance_list))
-            if (balance_list[i] == 0): break
+            min_trans = min(min_trans, 1 + dfs_backtrack(k + 1, balance_list, dp_hm))
+            #if (balance_list[i] == 0): break   ----> use this only if question asks for greedy solution
             balance_list[i] -= balance_list[k]
+    min_trans = 0 if (min_trans == inf) else min_trans
+    #dp_hm[k] = min_trans   ----> use this only if question asks for greedy solution
     return min_trans
 
 def settleDebt (arr):
-    person_acc_map = defaultdict(int)
-    for from, to, amt in arr: person_acc_map[from], person_acc_map[to] = person_acc_map[from] - amt, person_acc_map[to] + amt
-    balance_list = person_acc_map.values()
-    return dfs_backtrack(0, balance_list)
+    person_acc_map, dp_hm = defaultdict(int), {}
+    for send, rcv, amt in arr:
+        person_acc_map[send] -= amt ; person_acc_map[rcv] += amt
+    balance_list = [val for val in person_acc_map.values() if val != 0]
+    #print(balance_list)
+    return dfs_backtrack(0, balance_list, dp_hm)
