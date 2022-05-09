a8c5ea5c00376a8928deaabca1485c6580be25dd
Palash Sarkar <palashsarkar0007@gmail.com>
2022-05-09 18:44:06 +0530

    activity selection problem gfg link updated !!
    activity selection problem gfg link updated !!

--- Greedy/Activity Selection Problem/README.md
+++ Greedy/Activity Selection Problem/README.md
@@ -3,7 +3,7 @@
 ---
 
 ### Problem Links
-- **__GeeksForGeeks__** :point_right: https://practice.geeksforgeeks.org/problems/n-meetings-in-one-room-1587115620/1
+- **__GeeksForGeeks__** :point_right: https://practice.geeksforgeeks.org/problems/activity-selection-1587115620/1/
 
 ### Solution
-- **_solution.py_** :point_right: sort the given meetings with respect to start times, then use greedy technique **time-complexity O(Nlog(N))**, **space-complexity O(N)**.
+- **_solution.py_** :point_right: sort the given activities with respect to start times, then use greedy technique **time-complexity O(Nlog(N))**, **space-complexity O(N)**.
--- Greedy/Activity Selection Problem/solution.py
+++ Greedy/Activity Selection Problem/solution.py
@@ -1,12 +1,12 @@
 class Solution:
-    def maximumMeetings (self, n, start, end):
+    def activitySelection (self, n, start, end):
         if (n == 0): return 0
-        meetings = [(start[i], end[i]) for i in range(n)]
-        meetings.sort(key=lambda x: x[0])
+        activities = [(start[i], end[i]) for i in range(n)]
+        activities.sort(key=lambda x: x[0])
         i, j, c = 0, 1, 1
         while (j < n):
-            if (meetings[j][0] <= meetings[i][1]):
-                i = i if (meetings[j][1] > meetings[i][1]) else j
+            if (activities[j][0] <= activities[i][1]):
+                i = i if (activities[j][1] > activities[i][1]) else j
             else: i, c = j, c + 1
             j += 1
         return c
