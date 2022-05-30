a0c9b0dc7d033cd485ad0247b6ab7b90b2a7caf0
Palash Sarkar <palashsarkar0007@gmail.com>
2022-05-30 17:11:51 +0530

    commit !!
    commit !!

---
+++ Greedy/ARRANGE -Arranging Amplifiers/README.md
@@ -0,0 +1,9 @@
+# ARRANGE - Arranging Amplifiers
+
+---
+
+### Problem Links
+- **__SPOJ__** :point_right: https://www.spoj.com/problems/ARRANGE/
+
+### Solution
+- **_solution.py_** :point_right: use adhoc math logic **time-complexity O(Nlog(N))**, **space-complexity O(1)**.
--- Greedy/ARRANGE -Arranging Amplifiers/solution.py
+++ Greedy/ARRANGE -Arranging Amplifiers/solution.py
@@ -4,18 +4,20 @@ input = io.BytesIO(os.read(0, os.fstat(0).st_size)).readline
 def arrange ():
     T = int(input().decode())
     for t in range(T):
-        input()
-        m, n = map(int, input().decode().split(' '))
-        m, n = (m - 1), (n - 1)
-        cols = [int(input().decode()) for i in range(m)]
-        rows = [int(input().decode()) for i in range(n)]
-        cols.sort(reverse=True) ; rows.sort(reverse=True)
-        i, j, cost = 0, 0, 0
-        while ((i < m) and (j < n)):
-            if (cols[i] >= rows[j]): cost, i = (cost + (cols[i] * (j + 1))), (i + 1)
-            else: cost, j = (cost + (rows[j] * (i + 1))), (j + 1)
-        while (i < m): cost, i = (cost + (cols[i] * (j + 1))), (i + 1)
-        while (j < n): cost, j = (cost + (rows[j] * (i + 1))), (j + 1)
-        sys.stdout.write(str(cost) + '\n')
+        n = int(input().decode())
+        arr = list(map(int, input().decode().split(' ')))
+        arr.sort() ; i = 0
+        while ((i < n) and (arr[i] == 1)): i += 1
+        if (((i + 2) == n) and (arr[i] == 2) and (arr[i + 1] == 3)): pass
+        else:
+            j = n - 1
+            while (i < j):
+                arr[i], arr[j] = arr[j], arr[i]
+                i, j =  i + 1, j - 1
+        i = 0
+        while (i < n):
+            if (i != n - 1): sys.stdout.write(str(arr[i]) + ' ')
+            else: sys.stdout.write(str(arr[i]) + '\n')
+            i += 1
 
 arrange()
