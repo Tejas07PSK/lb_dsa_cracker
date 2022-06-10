718b44db3b7b68cb0f2e893b939e65eb642543e6
Palash Sarkar <palashsarkar0007@gmail.com>
2022-06-11 00:20:41 +0530

    commit !!
    commit !!

---
+++ Backtracking/Printing all solutions in N-Queen Problem/README.md
@@ -0,0 +1,9 @@
+# Printing all solutions in N-Queen Problem
+
+---
+
+### Problem Links
+- **__GeeksForGeeks__** :point_right: https://practice.geeksforgeeks.org/problems/n-queen-problem0315/1/
+
+### Solution
+- **_solution.py_** :point_right: use standard backtracking with recursion **time-complexity O(N!)**, **space-complexity O(N^2)**.
--- Backtracking/Printing all solutions in N-Queen Problem/solution.py
+++ Backtracking/Printing all solutions in N-Queen Problem/solution.py
@@ -1,17 +1,32 @@
 from collections import deque
 class Solution:
+    def __isSafe (self, row, col, n):
+        i, j = row - 1, col - 1
+        while ((i >= 0) and (j >= 0)):
+            if (self.chess_board[i][j] == True): return False
+            i, j = i - 1, j - 1
+        i, j = row - 1, col
+        while (i >= 0):
+            if (self.chess_board[i][j] == True): return False
+            i = i - 1
+        i, j = row - 1, col + 1
+        while ((i >= 0) and (j < n)):
+            if (self.chess_board[i][j] == True): return False
+            i, j = i - 1, j + 1
+        return True
+
     def __nQueenHelper (self, row, n):
         if (row == n):
-            self.ans.append(self.temp_ans.copy())
+            self.ans.append(list(self.temp_ans))
             return
         for col in range(n):
-            if (isSafe(row, col)):
-                self.chess_board[row][col] = True
-                self.temp_ans[row] = col + 1
+            if (self.__isSafe(row, col, n)):
+                self.chess_board[row][col] = True ; self.temp_ans.append(col + 1)
                 self.__nQueenHelper(row + 1, n)
-                self.chess_board[row][col] = False
-                self.temp_ans[row] = col + 1
+                self.chess_board[row][col] = False ; self.temp_ans.pop()
+
     def nQueen (self, n):
         self.ans, self.temp_ans = [], deque()
         self.chess_board = [[False for i in range(n)] for j in range(n)]
         self.__nQueenHelper(0, n)
+        return self.ans
