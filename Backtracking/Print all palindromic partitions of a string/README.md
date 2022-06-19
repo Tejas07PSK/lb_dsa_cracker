e379ec1fac04d49fc16903f0e05e2b4e7eaf0a31
Palash Sarkar <palashsarkar0007@gmail.com>
2022-06-20 02:58:31 +0530

    commit !!
    commit !!

---
+++ Backtracking/Print all palindromic partitions of a string/README.md
@@ -0,0 +1,9 @@
+# Print all palindromic partitions of a string
+
+---
+
+### Problem Links
+- **__GeeksForGeeks__** :point_right: https://practice.geeksforgeeks.org/problems/find-all-possible-palindromic-partitions-of-a-string/1/
+
+### Solution
+- **_solution.py_** :point_right: use standard backtracking algorithm **time-complexity O(N*2^N)**, **space-complexity O(N^2)**.
--- Backtracking/Print all palindromic partitions of a string/solution.py
+++ Backtracking/Print all palindromic partitions of a string/solution.py
@@ -1,1 +1,22 @@
-import
+class Solution:
+    def __isPalindrome (self, string):
+        i, j = 0, len(string) - 1
+        while (i < j):
+            if (string[i] != string[j]): return False
+            i, j = i + 1, j - 1
+        return True
+
+    def __allPalindromicPermsHelper (self, string_so_far, string):
+        if (string == ""):
+            self.res.append(string_so_far.split()) ; return
+        for i in range(1, len(string) + 1):
+            prefix_str = string[0:i]
+            if(self.__isPalindrome(prefix_str)):
+                suffix_str = string[i:]
+                ans = (prefix_str + " ") if (suffix_str != "") else prefix_str
+                self.__allPalindromicPermsHelper(string_so_far + ans, suffix_str)
+
+    def allPalindromicPerms (self, string):
+        self.res = []
+        self.__allPalindromicPermsHelper("", string)
+        return self.res
