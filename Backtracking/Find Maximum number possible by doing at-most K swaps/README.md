8c1cfbf4b86131748f336b76704fa3f7943cb3d9
Palash Sarkar <palashsarkar0007@gmail.com>
2022-07-06 00:29:34 +0530

    commit !!
    commit !!

---
+++ Backtracking/Find Maximum number possible by doing at-most K swaps/README.md
@@ -0,0 +1,9 @@
+# Find Maximum number possible by doing at-most K swaps
+
+---
+
+### Problem Links
+- **__GeeksForGeeks__** :point_right: https://practice.geeksforgeeks.org/problems/largest-number-in-k-swaps-1587115620/1/
+
+### Solution
+- **_solution.py_** :point_right: use ad-hoc logic with backtracking, look at code **time-complexity O(n^k)**, **space-complexity O(N)**.
--- Backtracking/Find Maximum number possible by doing at-most K swaps/solution.py
+++ Backtracking/Find Maximum number possible by doing at-most K swaps/solution.py
@@ -1,19 +1,19 @@
 class Solution:
-    def __findMaximumHelper (self, chr_arr, idx, k):
-        print(chr_arr) ; print(k) ; print(idx)
-        for i in range(idx, len(chr_arr)):
-            curr_max = chr_arr[i]
+    def __findMaximumHelper (self, chr_arr, i, k):
+        if ((i == len(chr_arr)) or (k ==0)): return
+        curr_max = chr_arr[i]
+        for j in range((i + 1), len(chr_arr)):
+            if (chr_arr[j] > curr_max): curr_max = chr_arr[j]
+        if (curr_max != chr_arr[i]):
             for j in range((i + 1), len(chr_arr)):
-                if (chr_arr[j] > curr_max): curr_max = chr_arr[j]
-            if (curr_max != chr_arr[i]):
-                for j in range((i + 1), len(chr_arr)):
-                    if (curr_max == chr_arr[j]):
-                        chr_arr[i], chr_arr[j] = chr_arr[j], chr_arr[i]
-                        if ((k - 1) != 0): self.__findMaximumHelper(chr_arr, i + 1, k - 1)
-                        else: self.ans = max(self.ans, int("".join(chr_arr)))
-                        chr_arr[i], chr_arr[j] = chr_arr[j], chr_arr[i]
-                        
-    def findMaximumNumber (self, string, k):
-        self.ans = 0
+                if (curr_max == chr_arr[j]):
+                    chr_arr[i], chr_arr[j] = chr_arr[j], chr_arr[i]
+                    self.ans = max(self.ans, int("".join(chr_arr)))
+                    self.__findMaximumHelper(chr_arr, i + 1, k - 1)
+                    chr_arr[i], chr_arr[j] = chr_arr[j], chr_arr[i]
+        else: self.__findMaximumHelper(chr_arr, i + 1, k)
+
+    def findMaximumNum (self, string, k):
+        self.ans = int(string)
         self.__findMaximumHelper(list(string), 0, k)
         return str(self.ans)
