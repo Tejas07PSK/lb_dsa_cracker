8bd9fa47d9d049ca958cdc15b4d2cc848fb3ef34
Palash Sarkar <palashsarkar0007@gmail.com>
2022-08-03 18:40:18 +0530

    commit !!
    commit !!

---
+++ Stacks & Queues/Largest rectangular Area in Histogram/README.md
@@ -0,0 +1,9 @@
+# Largest rectangular Area in Histogram
+
+---
+
+### Problem Links
+- **__GeeksForGeeks__** :point_right: https://practice.geeksforgeeks.org/problems/maximum-rectangular-area-in-a-histogram-1587115620/1
+
+### Solution
+- **_solution.py_** :point_right: use width expansion logic around each bar with the help of a stack, look at code **time-complexity O(N)**, **space-complexity O(N)**.
--- Stacks & Queues/Largest rectangular Area in Histogram/solution.py
+++ Stacks & Queues/Largest rectangular Area in Histogram/solution.py
@@ -1,15 +1,19 @@
-from collections from deque
+from collections import deque
 class Solution:
-    def getMaxArea (self, arr, n):
-        stk, ans = deque(), arr[0]
+    def getMaxArea (self, arr):
+        stk, ans, n = deque(), arr[0], len(arr)
         stk.append([arr[0], 1])
         for i in range(1, n):
             ans = max(ans, arr[i])
-            if (arr[i] <= stk[-1][0]):
-                prev_height, prev_left_spread = stk.pop()
-                stk.append([arr[i], 1 + prev_left_spread])
-                ans = max(ans, (stk[-1][0] * stk[-1][1]))
-            else: stk.append([arr[i], 1])
+            prev_height, prev_width = None, None
+            curr_height, curr_width = arr[i], 1
+            while ((stk) and (curr_height <= stk[-1][0])):
+                curr_width += stk[-1][1]
+                ans = max(ans, (curr_height * curr_width))
+                if (prev_height):
+                    stk[-1][1] += prev_width ; ans = max(ans, (stk[-1][0] * stk[-1][1]))
+                prev_height, prev_width = stk.pop()
+            stk.append([curr_height, curr_width])
         prev_height, prev_width = stk.pop()
         while (stk):
             stk[-1][1] += prev_width
