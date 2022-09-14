5fabb2c7828579bca4246192f8a26bc88660181c
Palash Sarkar <palashsarkar0007@gmail.com>
2022-09-15 01:59:47 +0530

    commit !!
    commit !!

--- Graph/Detect Cycle in UnDirected Graph using BFS and DFS Algo/solution1.py
+++ Graph/Detect Cycle in UnDirected Graph using BFS and DFS Algo/solution1.py
@@ -1,20 +1,17 @@
-from collections import deque
 class Solution:
     def __isCyclicHelper (self, idx, prev_idx, adj_lst):
-        self.visited[idx] = True ; self.curr_dfs_trace.add(idx)
+        self.visited[idx] = True
         for vert in adj_lst[idx]:
             if (not self.visited[vert]):
                 res = self.__isCyclicHelper(vert, idx, adj_lst)
                 if (res): return True
             else:
                 if (vert == idx): return True
-                if ((vert in self.curr_dfs_trace) and (vert != prev_idx)): return True
-        self.curr_dfs_trace.remove(idx)
+                if (vert != prev_idx): return True
         return False
 
     def isCycle (self, V, adj_lst):
         self.visited = [False for i in range(V)]
-        self.curr_dfs_trace = set()
         for i in range(V):
             if (not self.visited[i]):
                 res = self.__isCyclicHelper(i, None, adj_lst)
--- Graph/Detect Cycle in UnDirected Graph using BFS and DFS Algo/solution2.py
+++ Graph/Detect Cycle in UnDirected Graph using BFS and DFS Algo/solution2.py
@@ -2,25 +2,22 @@ from collections import deque
 class Solution:
     def __isCyclicHelper (self, idx, adj_lst):
         q = deque() ; q.append((idx, None))
-        self.visited[idx] = True ; self.curr_bfs_trace.add(idx)
+        self.visited[idx] = True
         while (q):
             node, prev_node = q.popleft()
             for vert in adj_lst[node]:
                 if (not self.visited[vert]):
                     self.visited[vert] = True
-                    self.curr_bfs_trace.add(vert)
                     q.append((vert, node))
                 else:
                     if (vert == node): return True
-                    if ((vert in self.curr_bfs_trace) and (vert != prev_node)): return True
+                    if (vert != prev_node): return True
         return False
 
     def isCycle (self, V, adj_lst):
         self.visited = [False for i in range(V)]
         for i in range(V):
             if (not self.visited[i]):
-                self.curr_bfs_trace = set()
                 res = self.__isCyclicHelper(i, adj_lst)
                 if (res == True): return True
-                del self.curr_bfs_trace
         return False
