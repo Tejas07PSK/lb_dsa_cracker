7bce47aef93348af26804effb4fc60f5b1528c1d
Palash Sarkar <palashsarkar0007@gmail.com>
2022-09-16 22:10:14 +0530

    commit !!
    commit !!

---
+++ Graph/Word Ladder/README.md
@@ -0,0 +1,9 @@
+# Word Ladder
+
+---
+
+### Problem Links
+- **__LeetCode__** :point_right: https://leetcode.com/problems/word-ladder/
+
+### Solution
+- **_solution.py_** :point_right: use standard bfs, by changing characters 1 at a time in the string at every stage **time-complexity O(NW^2)**, **space-complexity O(NW)**.
--- Graph/Word Ladder/solution.py
+++ Graph/Word Ladder/solution.py
@@ -1,23 +1,20 @@
 from collections import deque
 class Solution:
-    def __words_differ_by_1 (self, word1, word2):
-        j, count = 0, 0
-        for i in range(len(word1)):
-            if (word1[i] != word2[j]):
-                count += 1
-                if (count > 1): return False
-            j += 1
-        return True
-
     def ladderLength (self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        visited_word_states, wordList = set(), set(wordList)
         if (endWord not in wordList): return 0
-        visited_word_states = set()
-        q = deque() ; q.append((beginWord, 1)) ; visited_word_states.add(beginWord)
+        beginWord = list(beginWord)
+        q = deque() ; q.append((beginWord, 1)) ; visited_word_states.add("".join(beginWord))
         while (q):
             curr_word, steps_travelled = q.popleft()
-            for next_possible_word in wordList:
-                if ((next_possible_word not in visited_word_states) and self.__words_differ_by_1(curr_word, next_possible_word)):
-                    visited_word_states.add(next_possible_word)
-                    q.append((next_possible_word, (steps_travelled + 1)))
-                    if (next_possible_word == endWord): return steps_travelled + 1
+            for i in range(len(curr_word)):
+                ori_chr = curr_word[i]
+                for j in range(97, 123, 1):
+                    curr_word[i] = chr(j)
+                    next_possible_word = "".join(curr_word)
+                    if ((next_possible_word in wordList) and (next_possible_word not in visited_word_states)):
+                        if (next_possible_word == endWord): return steps_travelled + 1
+                        visited_word_states.add(next_possible_word)
+                        q.append((list(next_possible_word), (steps_travelled + 1)))
+                curr_word[i] = ori_chr
         return 0
