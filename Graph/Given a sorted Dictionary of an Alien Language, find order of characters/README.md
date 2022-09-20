ebd15461e53b7021493994fcd89b7b6a6efdfbd2
Palash Sarkar <palashsarkar0007@gmail.com>
2022-09-21 02:39:27 +0530

    commit !!
    commit !!

---
+++ Graph/Given a sorted Dictionary of an Alien Language, find order of characters/README.md
@@ -0,0 +1,9 @@
+# Given a sorted Dictionary of an Alien Language, find order of characters
+
+---
+
+### Problem Links
+- **__GeeksForGeeks__** :point_right: https://practice.geeksforgeeks.org/problems/alien-dictionary/1
+
+### Solution
+- **_solution1.py_** :point_right: refer to this video to understand the logic (https://www.youtube.com/watch?v=6kTZYvNNyps), once done with pre-processing use khan's top sort algo over each character **time-complexity O(NS+K) , where S denotes maximum length**, **space-complexity O(K)**.
--- Graph/Given a sorted Dictionary of an Alien Language, find order of characters/solution.py
+++ Graph/Given a sorted Dictionary of an Alien Language, find order of characters/solution.py
@@ -1,12 +1,25 @@
-from collections import defaultdict
+from collections import deque
 class Solution:
     def findOrder (self, dct, n, k):
-        adj_lst = defaultdict(lambda : [])
+        adj_lst, in_deg_hm = {chr(i):set() for i in range(97, 97+k)}, {chr(i):0 for i in range(97, 97+k)}
         for i in range(n - 1):
             string_a, string_b = dct[i], dct[i + 1]
             j, k = 0, 0
-            while ((i < len(string_a)) and (j < len(string_b))):
-                ch_a, ch_b = string_a[i], string_b[j]
+            while ((j < len(string_a)) and (k < len(string_b))):
+                ch_a, ch_b = string_a[j], string_b[k]
                 if (ch_a != ch_b):
-                    adj_lst[ch_a].append(ch_b)
+                    if (ch_b not in adj_lst[ch_a]):
+                        adj_lst[ch_a].add(ch_b)
+                        in_deg_hm[ch_b] += 1
                     break
+                j += 1 ; k += 1
+            if ((j < len(string_a)) and (k == len(string_b))): return ""
+        q, res, n = deque(), [], len(adj_lst)
+        for ch, in_deg in in_deg_hm.items():
+            if (in_deg == 0): q.append(ch)
+        while (q):
+            curr_char = q.popleft() ; res.append(curr_char)
+            for next_char in adj_lst[curr_char]:
+                in_deg_hm[next_char] -= 1
+                if (in_deg_hm[next_char] == 0): q.append(next_char)
+        return "".join(res) if (len(res) == n) else ""
