8a66c490dd34f60623f228b477ed21d2b1d9665c
Palash Sarkar <palashsarkar0007@gmail.com>
2022-09-26 23:51:20 +0530

    commit !!
    commit !!

---
+++ Graph/Total no. of Spanning tree in a graph/README.md
@@ -0,0 +1,9 @@
+# Total no. of Spanning tree in a graph
+
+---
+
+### Problem Links
+- **__CodeStudio__** :point_right: https://www.codingninjas.com/codestudio/problems/total-number-of-spanning-trees-in-a-graph_1171196
+
+### Solution
+- **_solution.py_** :point_right: use approach discussed here (https://www.codingninjas.com/codestudio/problem-details/total-number-of-spanning-trees-in-a-graph_1171196), look at code **time-complexity O(N^3)**, **space-complexity O(N^2)**.
--- Graph/Total no. of Spanning tree in a graph/solution.py
+++ Graph/Total no. of Spanning tree in a graph/solution.py
@@ -1,1 +1,23 @@
-from 
+def spanningTrees (adjMatrix, n, m):
+    edges, is_complete = 0, True
+    for i in range(n):
+        for j in range(n):
+            if (i != j):
+                if (adjMatrix[i][j] == 1):
+                    adjMatrix[j][j] += 1
+                    adjMatrix[i][j] = -1
+                    if (adjMatrix[j][i] != -1): edges += 1
+                else: is_complete = False
+    if (edges == (n - 1)): return 1
+    if (is_complete == True): return (n ** (n - 2))
+    factor, det = 1, 1
+    for i in range(1, n):
+        if (adjMatrix[i][i] == 0): return 0
+        det *= adjMatrix[i][i]
+        for j in range(i + 1, n):
+            if (adjMatrix[j][i] != 0):
+                factor *= adjMatrix[i][i]
+                temp = adjMatrix[j][i]
+                for k in range(i, n):
+                    adjMatrix[j][k] = ((adjMatrix[i][i] * adjMatrix[j][k]) - (temp * adjMatrix[i][k]))
+    return abs(det // factor)
