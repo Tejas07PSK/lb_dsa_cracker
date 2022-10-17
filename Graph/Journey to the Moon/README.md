714a3d9642e1eef9abb5690e8767f4c8d0db6460
Palash Sarkar <palashsarkar0007@gmail.com>
2022-10-18 02:08:26 +0530

    commit !!
    commit !!

---
+++ Graph/Journey to the Moon/README.md
@@ -0,0 +1,9 @@
+# Journey to the Moon
+
+---
+
+### Problem Links
+- **__HackerRank__** :point_right: https://www.hackerrank.com/challenges/journey-to-the-moon/problem
+
+### Solution
+- **_solution.py_** :point_right: use disjoint-set, with union-find technique **time-complexity O(Nlog(N))**, **space-complexity O(N)**.
--- Graph/Journey to the Moon/solution.py
+++ Graph/Journey to the Moon/solution.py
@@ -1,12 +1,23 @@
 class DisjointSet:
     def __init__ (self, n): self.parent = [[i, 1] for i in range(n)]
-    def find (self, node):
+    def __find (self, node):
         curr_node = node
         while (self.parent[curr_node][0] != curr_node): curr_node = self.parent[curr_node][0]
         self.parent[node][0] = curr_node
         return curr_node, self.parent[curr_node][1]
     def union (self, node1, node2):
-        parent1, size1, parent2, size2 = self.find(node1), self.find(node2)
-        if (size1 < seize2): parent2, parent1 = parent1, parent2
-        self.parent[parent2][0] = parent1
-        self.parent[parent2][1] += self.parent[parent2][1]
+        parent1, size1 = self.__find(node1)
+        parent2, size2 = self.__find(node2)
+        if (parent1 != parent2):
+            if (size1 < size2): parent2, parent1 = parent1, parent2
+            self.parent[parent2][0] = parent1
+            self.parent[parent1][1] += self.parent[parent2][1]
+
+def journeyToMoon (n, astronaut):
+    ds = DisjointSet(n)
+    for astro_id1, astro_id2 in astronaut: ds.union(astro_id1, astro_id2)
+    comp_size_counts = [ds.parent[i][1] for i in range(n) if (ds.parent[i][0] == i)]
+    s, res = sum(comp_size_counts), 0
+    for count in comp_size_counts:
+        s -= count ; res += (count * s)
+    return res
