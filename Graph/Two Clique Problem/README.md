0755e096226445b5b21c750446a8169c62c2e608
Palash Sarkar <palashsarkar0007@gmail.com>
2022-11-05 23:50:20 +0530

    commit !!
    commit !!

---
+++ Graph/Two Clique Problem/README.md
@@ -0,0 +1,9 @@
+# Two Clique Problem
+
+---
+
+### Problem Links
+- **__CodeStudio__** :point_right: https://www.codingninjas.com/codestudio/problems/two-cliques_1214524
+
+### Solution
+- **_solution.py_** :point_right: use standard 2 clique algorithm, first create a transpose/compliment of the given graph, then check if it is bi-partite or not, check code **time-complexity O(V+E)**, **space-complexity O(V^2)**.
--- Graph/Two Clique Problem/solution.py
+++ Graph/Two Clique Problem/solution.py
@@ -6,15 +6,18 @@ def twoCliques (n: int, edgeList: list):
     for start, end in edgeList:
         adj_mat[start][end] = 0
         adj_mat[end][start] = 0
-    color[0] = 0
-    stk = deque() ; stk.append((None, 0))
-    while (stk):
-        parent_node, curr_node = stk.popleft()
-        next_color = 1 if (color[curr_node] == 0) else 0
-        for next_node in range(n):
-            if ((adj_mat[curr_node][next_node] == 1) and (next_node != parent_node)):
-                if (color[curr_node] == color[next_node]): return False
-                if (color[next_node] == -1):
-                    color[next_node] = next_color
-                    stk.append((curr_node, next_node))
+    stk = deque()
+    for i in range(n):
+        if (color[i] == -1):
+            color[i] = 0
+            stk.append((None, i))
+            while (stk):
+                parent_node, curr_node = stk.popleft()
+                next_color = 1 if (color[curr_node] == 0) else 0
+                for next_node in range(n):
+                    if ((adj_mat[curr_node][next_node] == 1) and (next_node != parent_node)):
+                        if (color[curr_node] == color[next_node]): return False
+                        if (color[next_node] == -1):
+                            color[next_node] = next_color
+                            stk.append((curr_node, next_node))
     return True
