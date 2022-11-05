6c3e7f53faeefef8e55de619d4ea70f049ee2c1c
Palash Sarkar <palashsarkar0007@gmail.com>
2022-11-05 23:18:23 +0530

    commit !!
    commit !!

---
+++ Graph/Two Clique Problem/solution.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def twoCliques (n: int, edgeList: list):
+    color = [-1 for i in range(n)]
+    adj_mat = [[1 if (i != j) else 0 for i in range(n)] for j in range(n)]
+    for start, end in edgeList:
+        adj_mat[start][end] = 0
+        adj_mat[end][start] = 0
+    color[0] = 0
+    stk = deque() ; stk.append((None, 0))
+    while (stk):
+        parent_node, curr_node = stk.popleft()
+        next_color = 1 if (color[curr_node] == 0) else 0
+        for next_node in range(n):
+            if ((adj_mat[curr_node][next_node] == 1) and (next_node != parent_node)):
+                if (color[curr_node] == color[next_node]): return False
+                if (color[next_node] == -1):
+                    color[next_node] = next_color
+                    stk.append((curr_node, next_node))
+    return True
