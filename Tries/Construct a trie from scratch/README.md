c6d9c78edb1a8fe806702bee5b4f86317a1707c8
Palash Sarkar <palashsarkar0007@gmail.com>
2022-11-09 05:29:39 +0530

    commit !!
    commit !!

---
+++ Tries/Construct a trie from scratch/README.md
@@ -0,0 +1,9 @@
+# Construct a trie from scratch
+
+---
+
+### Problem Links
+- **__GeeksForGeeks__** :point_right: https://practice.geeksforgeeks.org/problems/trie-insert-and-search0651/1
+
+### Solution
+- **_solution.py_** :point_right: use straight-forward trie implementaion, with proper search and insert methods **time-complexity O(M+|search|)**, **space-complexity O(M)**, where M denotes sum of the lengths of all the strings present in input list and |search| denotes the length of the string search.
--- Tries/Construct a trie from scratch/solution.py
+++ Tries/Construct a trie from scratch/solution.py
@@ -1,1 +1,25 @@
-class 
+"""
+class TrieNode: 
+      
+    def __init__(self): 
+        self.children = [None]*26
+  
+        # isEndOfWord is True if node represent the end of the word 
+        self.isEndOfWord = False
+"""
+
+#Function to insert string into TRIE.
+def insert (root, key):
+    for ch in key:
+        ch_idx = ord(ch) - ord('a')
+        if (root.children[ch_idx] == None): root.children[ch_idx] = TrieNode()
+        root = root.children[ch_idx]
+    root.isEndOfWord = True
+
+#Function to use TRIE data structure and search the given string.
+def search (root, key):
+    for ch in key:
+        ch_idx = ord(ch) - ord('a')
+        if (root.children[ch_idx] == None): return False
+        root = root.children[ch_idx]
+    return root.isEndOfWord
