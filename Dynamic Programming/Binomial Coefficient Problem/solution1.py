6e822b52f8bba12fd11919e2a754e2beedcc3f56
Palash Sarkar <palashsarkar0007@gmail.com>
2022-11-24 00:25:42 +0530

    commit !!
    commit !!

--- Dynamic Programming/Binomial Coefficient Problem/solution1.py
+++ Dynamic Programming/Binomial Coefficient Problem/solution1.py
@@ -2,10 +2,13 @@ class Solution:
     def nCr (self, n, r):
         if (r > n): return 0
         if (r == n): return 1
-        if ((n - r) < r): r = n - r
-        dp = [0 for i in range(r + 1)] ; dp[0] = 1
         md = 1000000007
-        for i in range(1, (n + 1)):
-            for j in range(min(i, r), 0, -1):
-                dp[j] = (dp[j] + dp[j - 1]) % md
-        return dp[-1]
+        nmtr, denom, i = 1, 1, 0
+        while (i < r):
+            nmtr = (nmtr * (n - i))
+            i += 1
+        i = 1
+        while (i <= r):
+            denom = (denom * i)
+            i += 1
+        return ((nmtr // denom) % md)
