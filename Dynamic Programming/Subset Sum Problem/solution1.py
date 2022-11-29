f5b6170d66b5b5e4f506537d7684da2e10165c86
Palash Sarkar <palashsarkar0007@gmail.com>
2022-11-30 02:01:26 +0530

    commit !!
    commit !!

--- Dynamic Programming/Subset Sum Problem/solution1.py
+++ Dynamic Programming/Subset Sum Problem/solution1.py
@@ -1,13 +1,18 @@
 class Solution:
-    def __partition_helper (self, tot, i, s, n, arr):
-        if ((s - tot) == tot): return 1
-        if (i == n): return 0
-        if (self.dp[i][tot] == None):
-            self.dp[i][tot] = (self.__partition_helper(tot, i + 1, s, n, arr) or
-                               self.__partition_helper(tot + arr[i], i + 1, s, n, arr))
-        return self.dp[i][tot]
+    def __partition_helper (self, i, s, n, arr):
+        if (s == self.target): return 1
+        if (i == 0):
+            if ((s == self.target) or ((s - arr[i]) == self.target)): return 1
+            else: return 0
+        if (self.dp[i][s] == None):
+            self.dp[i][s] = self.__partition_helper(i - 1, s - arr[i], n, arr)
+            if (self.dp[i][s] == 0):
+                self.dp[i][s] = self.__partition_helper(i - 1, s, n, arr)
+        return self.dp[i][s]
 
     def equalPartition (self, n, arr):
         s = sum(arr)
-        self.dp = [[None for j in range(s + 1)] for i in rnage(n + 1)]
-        return self.__prtiton_helper(0, 0, s, n, arr)
+        if ((s % 2) != 0): return 0
+        self.target = s // 2
+        self.dp = [[None for j in range(s + 1)] for i in range(n)]
+        return self.__partition_helper(n - 1, s, n, arr)
