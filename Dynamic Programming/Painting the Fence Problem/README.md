7f1ac34683fefe21ecb3ffd459a18e5281de62fb
Palash Sarkar <palashsarkar0007@gmail.com>
2022-12-14 09:08:37 +0530

    commit !!
    commit !!

---
+++ Dynamic Programming/Painting the Fence Problem/README.md
@@ -0,0 +1,9 @@
+# Painting the Fence Problem
+
+---
+
+### Problem Links
+- **__GeeksForGeeks__** :point_right: https://practice.geeksforgeeks.org/problems/painting-the-fence3727/1
+
+### Solution
+- **_solution.py_** :point_right: use bottom up dp, with math logic **time-complexity O(N)**, **space-complexity O(1)**
--- Dynamic Programming/Painting the Fence Problem/solution.py
+++ Dynamic Programming/Painting the Fence Problem/solution.py
@@ -1,2 +1,12 @@
 class Solution:
-    
+    def countWays (self, n, k):
+        if (n == 1): return k
+        mod = 1000000007
+        same = k * 1
+        different =  k * (k - 1)
+        tot = same + different
+        for i in range(3, n + 1):
+            same = different * 1
+            different =  (tot * (k - 1)) % mod
+            tot = (same + different) % mod
+        return tot
