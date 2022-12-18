39ee19fb1de9b39465d31ebcd1e69671931185de
Palash Sarkar <palashsarkar0007@gmail.com>
2022-12-18 23:39:18 +0530

    commit !!
    commit !!

--- Dynamic Programming/Maximize The Cut Segments/solution.py
+++ Dynamic Programming/Maximize The Cut Segments/solution.py
@@ -1,24 +1,19 @@
-from math import inf
-
 class Solution:
     def __cut_helper(self, n):
         if (self.dp[n] == None):
+            #print("IN")
             self.dp[n] = 0
             for cut in self.cuts:
                 temp = 0
-                a = n // cut
-                if (a > 0):
-                    temp = a
-                    a = self.__cut_helper(cut)
-                    if (a > 0): temp *= a
-                    b =  n % cut
-                    if (b != 0): b = self.__cut_helper(b)
-                    if (b != 0): temp += b
-                    else: temp = 0
+                if ((n - cut) >= 0):
+                    temp = 1
+                    if ((n - cut) != 0): temp += self.__cut_helper(n - cut)
                 self.dp[n] = max(self.dp[n], temp)
         return self.dp[n]
 
     def maximizeTheCuts (self, n, x, y, z):
         self.cuts = [x, y, z]
-        self.dp = [None for i in range(1, n + 1)]
-        return self.__cut_helper(n)
+        self.dp = [None for i in range(n + 1)]
+        res = self.__cut_helper(n)
+
+print(Solution().maximizeTheCuts(100, 7, 12, 19))
