dd96827a1884046db38e0c2990c4ab14d7b2a1b3
Palash Sarkar <palashsarkar0007@gmail.com>
2023-01-17 03:14:53 +0530

    commit !!
    commit !!

---
+++ Dynamic Programming/Minimum number of jumps to reach end/README.md
@@ -0,0 +1,9 @@
+# Minimum number of jumps to reach end
+
+---
+
+### Problem Links
+- **__GeeksForGeeks__** :point_right: https://practice.geeksforgeeks.org/problems/minimum-number-of-jumps-1587115620/1
+
+### Solution
+- **_solution.py_** :point_right:  use simple ad-hoc greedy logic **time-complexity O(N)**, **space-complexity O(1)**
--- Dynamic Programming/Minimum number of jumps to reach end/solution.py
+++ Dynamic Programming/Minimum number of jumps to reach end/solution.py
@@ -3,19 +3,18 @@ class Solution:
     def minJumps (self, arr, n):
         i, jumps = 0, 0
         while (i < (n - 1)):
+            if (arr[i] == 0):
+                jumps = -1
+                break
+            jumps += 1
+            if ((i + arr[i]) >= (n - 1)): break
             j = i + 1
             rng_end = i + arr[i]
             i, min_diff = inf, inf
-            while (j <= rng_end ):
-                end_pos = j + arr[j]
-                if (end_pos >= (n - 1)):
-                    i = end_pos ; break
-                if (arr[end_pos] != 0):
-                    curr_diff = n - 1 - end_pos
-                    if (curr_diff <= min_diff):
-                        min_diff = curr_diff
-                        i = end_pos
+            while (j <= rng_end):
+                curr_diff = n - 1 - j - arr[j]
+                if (curr_diff <= min_diff):
+                    min_diff = curr_diff
+                    i = j
                 j += 1
-            if (i != inf): jumps += 1
-            else: jumps = -1
         return jumps
