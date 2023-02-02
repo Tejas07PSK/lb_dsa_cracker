281f2f4b8181a4047bd1b0f57187f2e4c799cd4c
Palash Sarkar <palashsarkar0007@gmail.com>
2023-02-02 22:29:40 +0530

    commit !!
    commit !!

---
+++ Dynamic Programming/Longest alternating subsequence/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+	def AlternatingaMaxLength (self, nums):
+		prev_lt_sz, prev_gt_sz = 1, 1
+		for i in range(1, len(nums)):
+		    if (nums[i] > nums[i - 1]): prev_gt_sz = prev_lt_sz + 1
+		    if (nums[i] < nums[i - 1]): prev_lt_sz = prev_gt_sz + 1
+		return max(prev_lt_sz, prev_gt_sz)
