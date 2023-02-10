e94bc445aaf996e30f388286f18343ed0de750a3
Palash Sarkar <palashsarkar0007@gmail.com>
2023-02-10 10:48:17 +0530

    commit !!
    commit !!

--- Dynamic Programming/Maximum profit by buying and selling a share at most k times/solution.py
+++ Dynamic Programming/Maximum profit by buying and selling a share at most k times/solution.py
@@ -1,1 +1,9 @@
-import
+class Solution:
+    def maxProfit(self, K, N, A):
+        dp = [[0 for i in range(N)] for j in range(K + 1)]
+        for i in range(1, K + 1):
+            mx_diff = -A[0]
+            for j in range(1, N):
+                dp[i][j] = max(dp[i][j - 1], A[j] + mx_diff)
+                mx_diff = max(mx_diff, dp[i - 1][j] - A[j])
+        return dp[-1][-1]
