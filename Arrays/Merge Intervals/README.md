2bffcb92096c8c1526957f94ee7de2f578c5836c
Palash Sarkar <palashsarkar0007@gmail.com>
2023-03-04 21:13:12 +0530

    commit !!
    commit !!

---
+++ Arrays/Merge Intervals/README.md
@@ -0,0 +1,9 @@
+# Merge Intervals
+
+---
+
+### Problem Links
+- **_LeetCode_** :point_right: https://leetcode.com/problems/merge-intervals/description/
+
+### Solution
+- **_solution.py_** :point_right: first sort the given intervals with respect to start times, then start merging them using simple logic with the help of a stack **time-complexity O(Nlog(N))**, **space-complexity O(N)**.
---
+++ Arrays/Merge Intervals/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def merge (self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x : x[0])
+        ans = deque() ; ans.append(intervals[0])
+        for i in range(1, len(intervals)):
+            if (intervals[i][0] <= ans[-1][1]): ans[-1][1] = max(ans[-1][1], intervals[i][1])
+            else: ans.append(intervals[i])
+        return ans
